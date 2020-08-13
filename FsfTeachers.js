7675a843c0d48633315f98c74fcc16d1dfdead67
wronghandedryan <wronghandedbarber@gmail.com>
2020-08-13 16:52:08 -0700

    1 file added, 1 deleted

--- FsfTeachers.js
+++ FsfTeachers.js
@@ -5,16 +5,16 @@ import {observer, inject} from 'mobx-react';
 
 @inject('store')
 @observer
-export default class fsfTeacher extends Component {
+export default class FsfTeacher() extends Component {
   constructor() {
     super();  
   }
   addHandler=()=>{
-   this.props.store.add('new fsfTeacher')
+   this.props.store.add('new FsfTeacher')
   }  
   render() {
     const {store}=this.props;
-    const items=store.fsfTeachers.map((item, i)=>
+    const items=store.FsfTeachers.map((item, i)=>
       <Item item={item} key={i} index={i} />);
 
     return <div style={{width:'500px'}}>
